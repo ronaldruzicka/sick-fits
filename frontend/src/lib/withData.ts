cd971b33aa91f10dfb51776419f4185be079afe0
Ronald Ruzicka <ruzicka.ronald@gmail.com>
2021-12-05 15:45:01 +0100

    feat: setup apollo client

--- frontend/src/lib/withData.ts
+++ frontend/src/lib/withData.ts
@@ -1,20 +1,48 @@
-import { ApolloClient } from 'apollo-boost';
-import withApollo from 'next-with-apollo';
+import { ApolloClient, ApolloLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import { getDataFromTree } from '@apollo/client/react/ssr';
+import { onError } from '@apollo/link-error';
+import { createUploadLink } from 'apollo-upload-client';
+import withApollo, { InitApolloOptions } from 'next-with-apollo';
 
-import { endpoint } from '../../config';
+import { endpoint, prodEndpoint } from '../config';
 
-function createClient({ headers }: any) {
+function createClient({ headers, initialState }: InitApolloOptions<NormalizedCacheObject>) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: (operation) => {
-      operation.setContext({
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+          );
+        }
+
+        if (networkError) {
+          console.log(`[Network error]: ${networkError}. Backend is unreachable. Is it running?`);
+        }
+      }),
+      // this uses apollo-link-http under the hood, so all the options here come from that package
+      createUploadLink({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
         fetchOptions: {
           credentials: 'include',
         },
+        // pass the headers along from this request. This enables SSR with logged in state
         headers,
-      });
-    },
+      }),
+    ]),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            // TODO: We will add this together!
+            // allProducts: paginationField(),
+          },
+        },
+      },
+    }).restore(initialState || {}),
   });
 }
 
-export const withData = withApollo(createClient);
+export default withApollo(createClient, { getDataFromTree });
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,44 @@
+import { ApolloClient, ApolloProvider, NormalizedCacheObject } from '@apollo/client';
 import { Page } from 'components/page';
+import 'components/styles/nprogress.css';
+import withData from 'lib/withData';
 import { AppProps } from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import 'components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+type ApolloProps = {
+  apollo: ApolloClient<NormalizedCacheObject>;
+};
+
+type Props = AppProps & ApolloProps;
+
+const MyApp = ({ Component, pageProps, apollo }: Props) => {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
-}
+};
+
+MyApp.getInitialProps = async ({ Component, ctx }: any) => {
+  if (typeof Component.getInitialProps === 'function') {
+    const pageProps = await Component.getInitialProps(ctx);
+
+    return {
+      ...pageProps,
+      query: ctx.query,
+    };
+  }
+
+  return {
+    query: ctx.query,
+  };
+};
+
+export default withData(MyApp);
